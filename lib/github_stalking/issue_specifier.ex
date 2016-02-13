d967435ed4ff25b6037780756882c5de3348dc12
letusfly85 <letusfly95@gmail.com>
2016-02-14 00:13:01 +0900

    add test case for issue_specifier

--- lib/github_stalking/issue_specifier.ex
+++ lib/github_stalking/issue_specifier.ex
@@ -1,21 +1,24 @@
 defmodule GithubStalking.IssueSpecifier do
+  @client Tentacat.Client.new
 
   @doc"""
   """
   def search_update_repositories(owner, repo, pre_issues) do
-    cur_issues = Tentacat.Issues.filter(owner, repo, @client)
+    cur_issues = Tentacat.Issues.filter(owner, repo, %{state: "open"}, @client)
 
-    issues = []
-    Enum.each(cur_issues, fn(cur_issue) ->
-      cur_number = cur_issue[:number]
-      pre_issue = pre_issues[cur_number]
+    Enum.filter(cur_issues, fn(cur_issue) ->
+      number = cur_issue["number"]
+      pre_issue = pre_issues[number]
+      
+      pre_issue != nil
+    end) |> Enum.reduce([], fn(cur_issue, issues) ->
+      number = cur_issue["number"]
+      pre_issue = pre_issues[number]
 
-      if cur_issue[:updated_at] > pre_issue[:update_at] do
-        issues = [cur_issue, issues]
+      if cur_issue["updated_at"] > pre_issue["update_at"] do
+        [cur_issue|issues]
       end
     end)
-
-    issues
   end
 
 end
---
+++ test/issue_specifier_test.exs
@@ -0,0 +1,11 @@
+defmodule GithubStalking.IssueSpecifierTest do
+  use ExUnit.Case
+
+  test "issue 1 should be updated after 2016-02-13T01:05:18Z" do
+    pre_issues = %{1 => %{"number" =>  1, "updated_at" => "2016-02-13T01:05:18Z"}} 
+    issues = GithubStalking.IssueSpecifier.search_update_repositories("letusfly85", "github_stalking", pre_issues)
+
+    assert length(issues) == 1
+    assert hd(issues)["title"] == "[module]search issue list from a github repository"
+  end
+end
