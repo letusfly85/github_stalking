09d1ede1749ae1d4e448189501f1fda0df9e7aa6
letusfly85 <letusfly95@gmail.com>
2016-02-18 08:14:34 +0900

    add get pre issue repositories test'

--- test/riak_test.exs
+++ test/riak_test.exs
@@ -4,6 +4,23 @@ defmodule GithubStalking.RiakTest do
   #GithubStalking.Riak.register(issues, "letusfly85", "github_stalking")
   #GithubStalking.Riak.register_numbers(issues, "letusfly85",  "github_stalking")
   #GithubStalking.Riak.register_numbers(issues, "letusfly105", "bitbucket_stalking")
+  test "get repos from issue_numbers" do
+    issues = [%{"number" => 11}, %{"number" => 12}, %{"number" => 13}]
+    GithubStalking.Riak.register_numbers(issues, "letusfly85",  "github_stalking")
+    GithubStalking.Riak.register_numbers(issues, "letusfly105", "bitbucket_stalking")
+
+    pre_issues_repos = GithubStalking.Riak.find_pre_issues_repos()
+    assert pre_issues_repos == ["letusfly105/bitbucket_stalking", "letusfly85/github_stalking"]
+  end
+
+  test "get unique issue from issue_history" do
+    issues = [%{"number" => 6}]
+    GithubStalking.Riak.register(issues, "letusfly85", "github_stalking")
+    obj = Riak.find(GithubStalking.Riak.get_pid, "issue_history", "letusfly85/github_stalking/6")
+    pre_issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
+
+    assert pre_issue.number == 6
+  end
 
   test "get issue numbers of a repository from issue_numbers" do
     issues = [%{"number" => 11}, %{"number" => 12}, %{"number" => 13}]
@@ -19,15 +36,6 @@ defmodule GithubStalking.RiakTest do
     end)
    
     assert [11, 12, 13] == list
-  end
-
-  test "get unique issue from issue_history" do
-    issues = [%{"number" => 6}]
-    GithubStalking.Riak.register(issues, "letusfly85", "github_stalking")
-    obj = Riak.find(GithubStalking.Riak.get_pid, "issue_history", "letusfly85/github_stalking/6")
-    pre_issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
-
-    assert pre_issue.number == 6
   end
 
   test "register issue numbers" do
