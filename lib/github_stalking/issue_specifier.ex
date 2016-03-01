938d12afefbbff8003079c18a21d16fefcf40969
letusfly85 <letusfly95@gmail.com>
2016-03-01 15:01:39 +0900

    remove IO.inspect and change to Logger.info

--- lib/github_stalking/issue_specifier.ex
+++ lib/github_stalking/issue_specifier.ex
@@ -27,7 +27,7 @@ defmodule GithubStalking.IssueSpecifier do
         {404, _} -> raise(owner <> "/" <> repo <> " doesn't have open issues.")
         _          -> 
 
-        Enum.reduce([], response, fn(current_issue, issues) ->
+        {:ok, Enum.reduce([], response, fn(current_issue, issues) ->
           number = current_issue["number"]
 
           case pre_issues[number] do
@@ -38,17 +38,17 @@ defmodule GithubStalking.IssueSpecifier do
                 _ -> issues
               end
           end
-        end)
+        end)}
       end
 
     rescue
       e in RuntimeError ->
             Logger.info(e.message)
-            []
+            {:error, []}
 
       e in UndefinedError ->
             Logger.info(e.message)
-            []
+            {:error, []}
     end
 
   end
@@ -60,18 +60,35 @@ defmodule GithubStalking.IssueSpecifier do
     {number, _} = pre_issue
     pre_issue_number = Integer.to_string(number)
     
-    Tentacat.Issues.find(owner, repo, pre_issue_number, @client) 
+    response = Tentacat.Issues.find(owner, repo, pre_issue_number, @client) 
+    case response do
+      {403, _} ->
+        Logger.info("it seems that you exceed limitation of GitHub API.")
+        {:error, []}
+      _ -> {:ok, response}
+    end
   end
 
   @doc"""
   search closed issues compared with pre searched
   """
   def closed_issues(owner, repo, pre_issues) do
-    Enum.filter(Map.to_list(pre_issues), fn(pre_issue) ->
-      cur_issue(owner, repo, pre_issue)["state"] == "closed"
-    end) |> Enum.reduce([], fn(pre_issue, issues) ->
-      [cur_issue(owner, repo, pre_issue)|issues]
+      closed_issue_list = Map.to_list(pre_issues) |> Enum.reduce([], fn(pre_issue, issues) ->
+      response = cur_issue(owner, repo, pre_issue)
+      case response do
+        {:ok, response} ->
+           case response["state"] do
+             "closed" ->
+               [response|issues]
+             _ ->
+               issues
+           end
+        {:error, _} ->
+           Logger.info("something wrong happen..")
+      end
     end)
+    
+    {:ok, closed_issue_list}
   end
 
   @doc"""
@@ -80,12 +97,17 @@ defmodule GithubStalking.IssueSpecifier do
     GithubStalking.Repository.target_repos()
     |> GithubStalking.Riak.issues_numbers
     |> Enum.each(fn(issue_numbers) ->
-         repo_full_path = issue_numbers.repo_full_path
-         pre_issues_map = GithubStalking.Riak.find_pre_issues_map(issue_numbers)
+        repo_full_path = issue_numbers.repo_full_path
+        pre_issues_map = GithubStalking.Riak.find_pre_issues_map(issue_numbers)
 
-         issues = updated_open_issues(repo_full_path, pre_issues_map)
-         GithubStalking.Riak.register(repo_full_path, issues)
-       end)
+        result = updated_open_issues(repo_full_path, pre_issues_map)
+        case result do
+          {:ok, issues} ->
+            GithubStalking.Riak.register(repo_full_path, issues)
+          {:error, _}   ->
+            GithubStalking.Riak.register(repo_full_path, [])
+        end
+      end)
   end
 
 end
--- test/issue_specifier_test.exs
+++ test/issue_specifier_test.exs
@@ -15,19 +15,29 @@ defmodule GithubStalking.IssueSpecifierTest do
     pre_issue = %GithubStalking.Issue{number: 22, updated_at: "2016-02-13T01:05:18Z"}
     pre_issues = %{22 => pre_issue} 
     
-    issues = GithubStalking.IssueSpecifier.updated_open_issues("letusfly85", "github_stalking", pre_issues)
+    response = GithubStalking.IssueSpecifier.updated_open_issues("letusfly85", "github_stalking", pre_issues)
+    case response do
+      {:ok, issues} -> 
+        assert length(Enum.sort(issues)) == 6
+        assert hd(Enum.sort(issues))["title"] == "travis ci settings"
+      {:error, _}   -> raise("connection to GitHub is refused!!!")
+    end
 
-    assert length(Enum.sort(issues)) == 6
-    assert hd(Enum.sort(issues))["title"] == "travis ci settings"
   end
 
   test "issue 6 should be updated after 2016-02-13T01:05:18Z" do
     pre_issue = %GithubStalking.Issue{number: 6}
     pre_issues = %{6 => pre_issue} 
-    issues = GithubStalking.IssueSpecifier.closed_issues("letusfly85", "github_stalking", pre_issues)
 
-    assert length(issues) >= 1
-    assert hd(issues)["title"] == "[module]search still open and updated issue list from a github repository"
+    response = GithubStalking.IssueSpecifier.closed_issues("letusfly85", "github_stalking", pre_issues)
+    case response do
+      {:ok, issues} -> 
+        assert length(issues) >= 1
+        IO.inspect(issues)
+        assert hd(issues)["title"] == "[module]search still open and updated issue list from a github repository"
+      {:error, _}   -> raise("connection to GitHub is refused!!!")
+    end
+
   end
 
   test "should collect one item from a repository" do
