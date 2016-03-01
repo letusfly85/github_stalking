4c8e5ea6a3b3edd0b481d16c16634feeac0e7244
letusfly85 <letusfly95@gmail.com>
2016-03-01 15:39:16 +0900

    remove nest

--- lib/github_stalking/issue.ex
+++ lib/github_stalking/issue.ex
@@ -8,26 +8,26 @@ defmodule GithubStalking.Issue do
 
   def show_issues(repo_full_path) do
     obj = Riak.find(GithubStalking.Riak.get_pid, "issue_numbers", repo_full_path)
+
+    result = nil 
     case obj do
       nil -> Logger.info("there is no issues")
       _   ->
         result = Poison.decode!(obj.data, as: %GithubStalking.Issues{})
-        case result.numbers do
-          [] -> Logger.info("there is no issues")
-          _  -> Enum.each(result.numbers, fn(number) ->
-                  path = repo_full_path <> "/" <> to_string(number)
-
-                  obj = Riak.find(GithubStalking.Riak.get_pid, "issue_history", path) 
-                  case obj do
-                    nil -> Logger.info("there is no issues")
-                    _ -> 
-                     issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
-                     Logger.info(issue)
-                  end
-                end)
-          end
     end
 
+    issue_numbers = Enum.filter(result.numbers, fn(numbers) -> numbers != [] end)
+    Enum.each(issue_numbers, fn(number) ->
+      path = repo_full_path <> "/" <> to_string(number)
+
+      obj = Riak.find(GithubStalking.Riak.get_pid, "issue_history", path) 
+      case obj do
+        nil -> Logger.info("there is no issues")
+        _ -> 
+         issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
+         Logger.info(issue)
+      end
+    end)
   end
 
 end
