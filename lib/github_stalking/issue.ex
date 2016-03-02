a8326de66bfa0242ce67727f8fe580274ee04d12
letusfly85 <letusfly95@gmail.com>
2016-03-02 23:59:30 +0900

    add slack post method

--- lib/github_stalking/issue.ex
+++ lib/github_stalking/issue.ex
@@ -11,24 +11,26 @@ defmodule GithubStalking.Issue do
 
     result = nil 
     case obj do
-      nil -> Logger.info("there is no issues")
+      nil -> Logger.info(repo_full_path <> " doesn't have any issues")
       _   ->
         result = Poison.decode!(obj.data, as: %GithubStalking.Issues{})
     end
 
     issue_numbers = Enum.filter(result.numbers, fn(numbers) -> numbers != [] end)
-    Enum.each(issue_numbers, fn(number) ->
+    Enum.reduce(issue_numbers, [], fn(number, issues) ->
       path = repo_full_path <> "/" <> to_string(number)
 
       obj = Riak.find(GithubStalking.Riak.get_pid, "issue_history", path) 
       case obj do
-        nil -> Logger.info("there is no issues")
-        _ -> 
-         issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
-         Logger.info(issue)
+        nil ->
+          Logger.info("cannot get info from " <> path)
+          issues
+        _   -> 
+          issue = Poison.decode!(obj.data, as: %GithubStalking.Issue{})
+          Logger.info(issue.title)
+          [issue|issues]
       end
     end)
   end
 
 end
-
--- lib/github_stalking/slack.ex
+++ lib/github_stalking/slack.ex
@@ -3,11 +3,30 @@ defmodule GithubStalking.Slack do
   """
   require Logger
 
+  @slack_webhook_url System.get_env("slack_webhook_url")
+
   @doc"""
   """
   def notify_update_issues do
     #TODO
   end
 
-end
+  @doc"""
+  """
+  def notify_update_issues(repo_full_path) do
+    issues = GithubStalking.Issue.show_issues(repo_full_path)
+    headers = []
+    Logger.info(@slack_webhook_url)
+    Enum.each(issues, fn(issue) ->
+      text = repo_full_path <> ", title: " <> issue.title <> ", updated_at: " <> issue.updated_at 
+      json_data = %{channel:    "#github_extra", 
+                    username:   "github_extra",
+                    text:       text,
+                    icon_emoji: ":ghost:"} |> Poison.encode!
 
+      response = HTTPoison.post!(@slack_webhook_url, json_data, headers)
+      Logger.info "response: #{inspect response}"
+    end)
+  end
+
+end
