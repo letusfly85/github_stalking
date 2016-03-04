78077b25dcef7ff9c83bfe9a9a25db8dd52b52a8
letusfly85 <letusfly95@gmail.com>
2016-03-04 17:34:45 +0900

    add thumb_image_url to issue struct

--- lib/github_stalking/slack/slack.ex
+++ lib/github_stalking/slack/slack.ex
@@ -13,18 +13,37 @@ defmodule GithubStalking.Slack do
 
   @doc"""
   """
+  def generate_json_data(repo_full_path, issue) do
+    repo_full_path_with_number = repo_full_path   <> "/issues/" <> Integer.to_string(issue.number) 
+    text = issue.title <>
+           "\nupdated_at: " <> issue.updated_at
+
+    linked_text = "<https://github.com/" <> repo_full_path_with_number <>
+                  "|" <> repo_full_path_with_number <> ">"
+ 
+    json_data = %{channel:    "#github_extra", 
+                  username:   "github_extra",
+                  text:       linked_text,
+                  attachments: [
+                    %{color:    "#36a64f",
+                      text: text,
+                      #image_url: issue.avatar_url,
+                      thumb_url: issue.avatar_url
+                    }
+                  ],
+                  icon_emoji: ":ghost:"} |> Poison.encode!
+    json_data
+  end
+
+  @doc"""
+  """
   def notify_update_issues(repo_full_path) do
     issues = GithubStalking.Github.Issue.find_issues(repo_full_path)
     headers = []
     Logger.info(System.get_env("slack_webhook_url"))
     Logger.info(@slack_webhook_url)
     result = Enum.reduce(issues, [], fn(issue, acc) ->
-      text = "https://github.com/" <> repo_full_path <> "/issues/" <> Integer.to_string(issue.number) <> "\ntitle: " <> issue.title <> ", updated_at: " <> issue.updated_at 
-      json_data = %{channel:    "#github_extra", 
-                    username:   "github_extra",
-                    text:       text,
-                    icon_emoji: ":ghost:"} |> Poison.encode!
-
+      json_data = generate_json_data(repo_full_path, issue) 
       response = HTTPoison.post!(@slack_webhook_url, json_data, headers)
       Logger.info "response: #{inspect response}"
       Map.merge(issue, %GithubStalking.Github.Issue{is_notified: true})
