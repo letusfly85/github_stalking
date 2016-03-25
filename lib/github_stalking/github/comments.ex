f90d5a557aa563d5a1e47c76f90b9227b3e71b08
letusfly85 <letusfly95@gmail.com>
2016-03-25 18:53:53 +0900

    modify test case

--- lib/github_stalking/github/comments.ex
+++ lib/github_stalking/github/comments.ex
@@ -12,7 +12,8 @@ defmodule GithubStalking.Github.Comments do
   """
   def aggregate_comments(comments) do
     comment_count = length(comments)
-    participants   = Enum.unique(Enum.map(comments, fn(comment) -> comment.login end))
+    dup_participants = Enum.map(comments, fn(comment) -> comment.login end)
+    participants   = dup_participants |> Enum.uniq |> Enum.sort
     
     %GithubStalking.Github.Comments{number: hd(comments).number, comment_count: comment_count, participants: participants, comments: comments}
   end
