4687b656b485d4a43eded5034189af7ab59e475b
letusfly85 <letusfly95@gmail.com>
2016-04-03 08:36:25 +0900

    typo

--- lib/github_stalking/slack/slack.ex
+++ lib/github_stalking/slack/slack.ex
@@ -12,8 +12,17 @@ defmodule GithubStalking.Slack do
 
     linked_text = "<https://github.com/" <> repo_full_path_with_number <>
                   "|" <> repo_full_path_with_number <> ">"
-    comment_detail = "comment count:       " + Integer.to_string(issue.comments.comment_count) <> "\n" <>
-                     "participant_count:   " + Integer.to_string(issue.comments.participant_count)
+    comment_detail = ""
+    comment_color  = "yellow"
+    case issue.comments do
+      [] -> 
+        comment_detail = repo_full_path_with_number <> " doesn't have comments yet..."
+        comment_color = "#d29ac6"
+      _  -> 
+        comment_detail = "comment count:       " + Integer.to_string(issue.comments.comment_count) <> "\n" <>
+                         "participant_count:   " + Integer.to_string(issue.comments.participant_count)
+        comment_color = "#ddd6ca"
+    end
  
     json_data = %{channel:    "#github_extra", 
                   username:   "github_extra",
@@ -24,7 +33,7 @@ defmodule GithubStalking.Slack do
                       #image_url: issue.avatar_url,
                       thumb_url: issue.avatar_url
                     },
-                    %{color:    "yellow",
+                    %{color:    comment_color,
                       text: comment_detail
                     }
                   ],
